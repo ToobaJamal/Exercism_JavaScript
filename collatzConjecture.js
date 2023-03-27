9c09119edd190059fbc0692972da266c51a42eb8
Tooba Jamal <52610124+ToobaJamal@users.noreply.github.com>
2023-03-27 14:03:30 +0500

    Add instructions

--- collatzConjecture.js
+++ collatzConjecture.js
@@ -1,3 +1,8 @@
+/*
+Given a number n, return the number of steps required to reach 1.
+If n is not a positive integer, stop the program from being executed further and return an error message.
+*/
+
 const steps = (n) => {
     if(n <= 0) {
         throw new Error('Only positive numbers are allowed')

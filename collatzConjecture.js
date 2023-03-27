bfd768fefd4d87e708f3f2139dc95186b387fa37
Tooba Jamal <52610124+ToobaJamal@users.noreply.github.com>
2023-03-27 14:02:43 +0500

    Add code

--- collatzConjecture.js
+++ collatzConjecture.js
@@ -1,1 +1,17 @@
-
+const steps = (n) => {
+    if(n <= 0) {
+        throw new Error('Only positive numbers are allowed')
+    }
+    let num = n
+  let steps = 0
+  while(num > 1) {
+      steps++
+      if(num%2 === 0) {
+          num = num/2
+      }
+      else {
+          num = (num * 3) + 1
+      }
+  }
+  return steps
+};
